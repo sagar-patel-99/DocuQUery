8bfc6c01e01d26dae0c0a41deb9bc614567b2b62
Sagar Patel <52280059+sagar-patel-99@users.noreply.github.com>
2023-11-20 19:58:01 -0800

    Add files via upload

---
+++ answer.py
@@ -0,0 +1,8 @@
+response_template = '''
+<div class="chat-message response">
+    <div class="prompt">
+        <img src="https://www.picng.com/upload/letter_a/png_letter_a_50149.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="response">{{MSG}}</div>
+</div>
+'''
---
+++ userinterface.py
@@ -0,0 +1,28 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.prompt {
+    background-color: #2b313e
+}
+.chat-message.response {
+    background-color: #475063
+}
+
+.chat-message .prompt img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .response {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+
+
+
